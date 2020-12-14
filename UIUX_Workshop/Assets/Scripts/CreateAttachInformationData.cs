9f8289eefc967a568bb76bfd1727e18ec4b936ef
yvens <yvensre@gmail.com>
2020-12-14 20:04:54 +0100

    Create information using an InputField

--- UIUX_Workshop/Assets/Scripts/CreateAttachInformationData.cs
+++ UIUX_Workshop/Assets/Scripts/CreateAttachInformationData.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CreateAttachInformationData : MonoBehaviour
 {
     public InformationData objectToBeCreated;
     public GameObject objectToBeAttached;
+    public InputField inputField;
 
-    public void CreateAttachWithText(string text)
+    public void CreateAttachWithText()
     {
         if (objectToBeCreated != null && objectToBeAttached != null)
         {
             var info = Instantiate(objectToBeCreated, 
                 objectToBeAttached.transform);
-            info.Initialize(text);
+            var trimmedText = inputField.text.Trim();
+            if (trimmedText.Length > 0)
+            {
+                info.Initialize(trimmedText);    
+            }
+            inputField.text = "";
         }
     }
 }
